4c135eb82eeb8178e279997c089a1b279932f8ef
Lasha Magaldadze <lashagiorgi420@gmail.com>
2024-11-15 07:13:33 +0400

    route for retrieving session by sessionId

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -18,8 +18,9 @@ export const createPaymentIntent: RequestHandler = async (
         product_data: {
           name: product.geo_title,
           description: product.geo_description,
+          images: product.images,
         },
-        unit_amount: product.price,
+        unit_amount: product.price * 100,
       },
       quantity: product.qty || 1,
     }))
@@ -28,13 +29,38 @@ export const createPaymentIntent: RequestHandler = async (
       payment_method_types: ['card'],
       line_items: lineItems,
       mode: 'payment',
-      success_url: `${process.env.STRIPE_SUCCESS_URL}`,
+      success_url: `${process.env.STRIPE_SUCCESS_URL}?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: process.env.ORIGIN,
+      shipping_address_collection: {
+        allowed_countries: ['GE'],
+      },
     })
 
     res.status(200).json({ sessionId: session.id })
   } catch (error) {
     console.error('Error creating Checkout Session:', error)
+    res.status(500).send({ error: error.message })
+  }
+}
+
+export const retrieveSession: RequestHandler = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  try {
+    const { sessionId } = req.params
+
+    const session = await stripe.checkout.sessions.retrieve(sessionId)
+    const lineItems = await stripe.checkout.sessions.listLineItems(sessionId)
+
+    if (session.payment_status === 'paid') {
+      res.status(200).json({ status: 'success', session, lineItems })
+    } else {
+      res.status(200).json({ status: 'failed', session, lineItems })
+    }
+  } catch (error) {
+    console.error('Error retrieving session:', error)
     res.status(500).send({ error: error.message })
   }
 }
--- src/routers/stripe.router.ts
+++ src/routers/stripe.router.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express'
-import { createPaymentIntent } from '../controllers/stripe.controller.js'
+import {
+  createPaymentIntent,
+  retrieveSession,
+} from '../controllers/stripe.controller.js'
 
 const stripeRouter = Router()
 
 stripeRouter.post('/create/payment/intent', createPaymentIntent)
+stripeRouter.get('/session/get/:sessionId', retrieveSession)
 
 export default stripeRouter
